dfbb42631efbffcc0d6afc5ebd77a8d9914af08b
yashasri <yash@yashasris-mbp.dhcp.mc.vanderbilt.edu>
2017-05-02 14:57:37 -0500

    task4

--- hello_world.js
+++ hello_world.js
@@ -1,1 +1,7 @@
-console.log('hello world');
+
+ document.getElementById("row1").style.background = "blue"
+ document.getElementById("row2").style.background = "green"
+ document.getElementById("row3").style.background = "red"
+ document.getElementById("row4").style.background = "yellow"
+ document.getElementById("row5").style.background = "orange"
+ document.getElementById("row6").style.background = "pink"

8c74cdbe2bf09646bb4442bfd7e0af5eb65b726f
yashasri <yash@yashasris-mbp.dhcp.mc.vanderbilt.edu>
2017-05-08 13:10:17 -0500

    task4

--- hello_world.js
+++ hello_world.js
@@ -1,7 +1,6 @@
+var rows = document.getElementsByTagName('tr');
 
- document.getElementById("row1").style.background = "blue"
- document.getElementById("row2").style.background = "green"
- document.getElementById("row3").style.background = "red"
- document.getElementById("row4").style.background = "yellow"
- document.getElementById("row5").style.background = "orange"
- document.getElementById("row6").style.background = "pink"
+var colorArray = ['', 'blue', 'green', 'red', 'yellow', 'orange']
+for (var rowKey = 1; rowKey < rows.length; rowKey++) {
+    rows[rowKey].style.backgroundColor = colorArray[rowKey];
+}

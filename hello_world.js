92d12a0e8ed2deb3abd84ebf00dbd4ee940b0458
yashasri <yash@yashasris-mbp.dhcp.mc.vanderbilt.edu>
2017-05-19 14:24:19 -0500

    task5

--- hello_world.js
+++ hello_world.js
@@ -1,35 +1,45 @@
-angular.module('helloWorldApp', []).
- controller('HelloWorldController', ['$scope', '$http', function HelloWorldController ($scope, $http)
- {
+angular.module("helloWorldApp", [])
 
-    $http.get('http://yash.dev.hello-world.com/hello_world.php').then(function(response) {
+    .directive('helloWorld', [
 
-        $scope.countries = response.data;
+        function () {
 
-        var colorArray = ['green', 'yellow', 'red', 'blue', 'pink']
-        var currentIndex = 0;
+            return {
+                scope: {reverse: '='},
+                restrict: 'E',
+                templateUrl: "temp1.html",
+                controller: function ($scope, $http) {
 
-        angular.forEach($scope.countries,function(country) {
+                    var colorArray = [], currentIndex = 0;
 
-            country.backgroundColor = colorArray[currentIndex];
+                    $http.get('http://yash.dev.hello-world.com/hello_world.php').then(function(response) {
 
-            currentIndex++;
+                        $scope.countries = response.data;
 
-            if (currentIndex == colorArray.length) {
-                    currentIndex = 0;
+                        colorArray = ['green', 'yellow', 'red', 'blue', 'pink']
+
+                        if ($scope.reverse == true)
+                        {
+                           colorArray.reverse()
+                        }
+
+                        angular.forEach($scope.countries,function(country) {
+
+                            country.backgroundColor = colorArray[currentIndex];
+
+                            currentIndex++;
+
+                            if (currentIndex == colorArray.length) {
+                                    currentIndex = 0;
+                            }
+
+                        })
+                    });
+
+                }
             }
+        }
+    ])
 
-        });
-    });
+;
 
-
-
-    // var rows = angular.element($document[0].getElementsByTagName('tr'));
-    // console.log($document[0].getElementsByTagName('tr'));
-    // // var rows = angular.element(row);
-
-    // var colorArray = ['green', 'yellow', 'red', 'blue', 'pink']
-    // for (var rowKey = 1; rowKey < rows.length; rowKey++) {
-    //     rows[rowKey].style.backgroundColor = colorArray[rowKey];
-    // }
-}]);
